59c4388b3dda912e39a53962a0c4ac9e15102f6c
zubby peculiar <zubbypeculiar@gmail.com>
2023-09-12 21:54:12 +0100

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/ruc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+	int retval = 1;
+	if(n!=1) {
+		retval = n * factorial(n-1);
+	}
+	return retval;
+}
+
+int main(void)
+{
+	int val = factorial(5);
+	printf("%d! = %d\n",5,val);
+	
+	printf("%d! = %d\n",4,factorial(4));
+}
