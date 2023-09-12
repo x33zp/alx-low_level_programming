84fda4d6f2a20fe5623efe7d4487a322c356578b
zubby peculiar <zubbypeculiar@gmail.com>
2023-09-13 00:23:24 +0100

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* int _islower - check the code
+*
+* Return: 1 and 0 depending on the condition
+*/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,14 +2,17 @@
 #define MAIN_H
 
 /*
- * File - main.h
- * Author - Daniel John
- * Description : Header contains prototype for all functions
- *		used in 0x02-functions_nested_loops directory
- */
+* File - main.h
+*
+* Author - Zubby Peculiar
+*
+* Description : Header contains prototype for all functions
+*		used in 0x02-functions_nested_loops directory
+*/
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif
