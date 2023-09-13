fa00183c449ccae3312ff2691995a9ebf7232781
zubby peculiar <zubbypeculiar@gmail.com>
2023-09-13 01:24:14 +0100

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,26 +3,26 @@
 /**
 * print_sign - Checks code
 *
+* @n: character arguement
+*
 * Return: 1, 0 or -1 depending on the condition
 */
 
 int print_sign(int n)
 {
 	if (n > 0)
-		{
-			_putchar('+');
-			return (1);
-		}
-
+	{
+		_putchar('+');
+		return (1);
+	}
 	else if (n < 0)
-                {
-                        _putchar('-');
-                        return (-1);
-                }
-
+	{
+		_putchar('-');
+		return (-1);
+	}
 	else
-                {
-                        _putchar(48);
-                        return (0);
-                }
+	{
+		_putchar(48);
+		return (0);
+	}
 }
