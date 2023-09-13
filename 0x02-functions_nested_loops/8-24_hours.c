42c382e6b7a8973a83ce565ef86289336eab0183
zubby peculiar <zubbypeculiar@gmail.com>
2023-09-13 03:19:28 +0100

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * jack_bauer - prints every minute of the day of
  *		Jack Bauer, starting from 00:00 to
  *		23:59
  * Return: Always 0.
  */
+
 void jack_bauer(void)
 {
 	int a, b;
@@ -18,27 +20,23 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(a + '0');
 			}
-			
 			else if (a >= 10)
 			{
 				_putchar((a / 10) + '0');
 				_putchar((a % 10) + '0');
 			}
-
 			if (b < 10)
 			{
 				_putchar(':');
 				_putchar('0');
 				_putchar(b + '0');
 			}
-
 			else if (b >= 10)
 			{
 				_putchar(':');
 				_putchar((b / 10) + '0');
 				_putchar((b % 10) + '0');
 			}
-				
 			_putchar('\n');
 
 		}
