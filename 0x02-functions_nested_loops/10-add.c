4c8c0166a5384af4ed0b0209cc320343fe3468b9
zubby peculiar <zubbypeculiar@gmail.com>
2023-09-13 03:32:53 +0100

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ *
+ * @a: character arguement
+ *
+ * @b: character arguement
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
