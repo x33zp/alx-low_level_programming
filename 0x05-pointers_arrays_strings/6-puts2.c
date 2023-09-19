f59b13aa2cb9b3e902675d5b03beec7679fd796f
zubby peculiar <zubbypeculiar@gmail.com>
2023-09-19 23:12:34 +0100

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every other character of a string,
+ *	  starting with the first character.
+ *
+ * str: argument
+ *
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
