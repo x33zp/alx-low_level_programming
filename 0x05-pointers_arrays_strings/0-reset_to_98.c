a2790abda955fd6d31a0a41056494d50dbc506e1
zubby peculiar <zubbypeculiar@gmail.com>
2023-09-19 16:50:16 +0100

    a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98(int *n) - function that takes a pointer to an int as parameter
+ * updates the value it points to to 98.
+ *
+ * 
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author - Daniel John
+ * Description : Header contains prototype for all functions
+ *		used in 0x02-functions_nested_loops directory
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
