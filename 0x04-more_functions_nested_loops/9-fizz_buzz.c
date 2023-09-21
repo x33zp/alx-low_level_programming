566f399ff76c29b15ffe11f96e9b5f59cc141343
zubby peculiar <zubbypeculiar@gmail.com>
2023-09-21 12:21:42 +0100

    9-

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Print fizz, buz or fizzbuzz
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if(a % 5 == 0 )
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
