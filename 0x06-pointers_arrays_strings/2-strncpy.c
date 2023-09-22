6c975656d0330241664a67fbc819a1df28ff08bd
zubby peculiar <zubbypeculiar@gmail.com>
2023-09-22 01:24:05 +0100

    char *_strncpy(char *dest, char *src, int n)

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings.
+ *
+ * @dest: argument
+ * @src: argument
+ * @n: argument
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; n > i; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
